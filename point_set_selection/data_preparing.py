51b4624a1e71f153f55a09dc6a2735972c675764
Lin Shao <lins2@bohg-ws-6.stanford.edu>
2020-09-14 01:38:55 -0700

    update data_preparing

--- point_set_selection/data_preparing.py
+++ point_set_selection/data_preparing.py
@@ -6,20 +6,18 @@ sys.path.append(os.path.join(BASE_DIR,os.pardir))
 from Train_Val_Test import *
 import numpy as np
 np.random.seed(42)
-data_top_dir = os.path.join('../data/objects/train_set')
+data_top_dir = os.path.join('../data/train_data')
 total_ids = []
 
-for line_id in os.listdir(data_top_dir)[:100]:
+for line_id in os.listdir(data_top_dir):
   sub_dir = os.path.join(data_top_dir,line_id)
-  print(sub_dir)
-  f1 = [line for line in os.listdir(sub_dir) if line.endswith('grasp1_new.npz')]
-  f2 = [line for line in os.listdir(sub_dir) if line.endswith('grasp2_new.npz')]
-  f3 = [line for line in os.listdir(sub_dir) if line.endswith('grasp3_new.npz')]
-  f4 = [line for line in os.listdir(sub_dir) if line.endswith('grasp4_new.npz')]
-  f5 = [line for line in os.listdir(sub_dir) if line.endswith('grasp5_new.npz')]
-  if len(f4) > 0 and len(f2) > 0 and len(f3) > 0 and len(f1) > 0 and len(f5) > 0:
-    full_path = os.path.join(sub_dir,f1[0])
-    np.load(full_path)
+  #print(sub_dir)
+  #f1 = [line for line in os.listdir(sub_dir) if line.endswith('grasp1_new.npz')]
+  #f2 = [line for line in os.listdir(sub_dir) if line.endswith('grasp2_new.npz')]
+  #f3 = [line for line in os.listdir(sub_dir) if line.endswith('grasp3_new.npz')]
+  #f4 = [line for line in os.listdir(sub_dir) if line.endswith('grasp4_new.npz')]
+  #f5 = [line for line in os.listdir(sub_dir) if line.endswith('grasp5_new.npz')]
+  if 1:#len(f4) > 0 and len(f2) > 0 and len(f3) > 0 and len(f1) > 0 and len(f5) > 0:
     total_ids.append(line_id)
 
 train_val_test_list = Train_Val_Test(np.array(total_ids),splitting=[1.0,0.0,0.0])
--- point_set_selection/data_preparing_test.py
+++ point_set_selection/data_preparing_test.py
@@ -6,16 +6,16 @@ sys.path.append(os.path.join(BASE_DIR,os.pardir))
 from Train_Val_Test import *
 import numpy as np
 np.random.seed(42)
-data_top_dir = os.path.join('../data/objects')
+data_top_dir = os.path.join('../data/test_data')
 total_ids = []
 
 for line_id in os.listdir(data_top_dir):
   sub_dir = os.path.join(data_top_dir,line_id)
-  print(sub_dir)
-  f3 = [line for line in os.listdir(sub_dir) if line.endswith('grasp3_new.npz')]
-  f4 = [line for line in os.listdir(sub_dir) if line.endswith('grasp4_new.npz')]
-  f5 = [line for line in os.listdir(sub_dir) if line.endswith('grasp5_new.npz')]
-  print("len1",len(f3),len(f4),len(f5))
+  #print(sub_dir)
+  #f3 = [line for line in os.listdir(sub_dir) if line.endswith('grasp3_new.npz')]
+  #f4 = [line for line in os.listdir(sub_dir) if line.endswith('grasp4_new.npz')]
+  #f5 = [line for line in os.listdir(sub_dir) if line.endswith('grasp5_new.npz')]
+  #print("len1",len(f3),len(f4),len(f5))
   total_ids.append(line_id)
 
 train_val_test_list = Train_Val_Test(np.array(total_ids),splitting=[1.0,0.0,0.0])
